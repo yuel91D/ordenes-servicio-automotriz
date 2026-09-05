fc79017150f3495d0ac26998762dac752492ddf7
Yuel91 <yueldevops@gmail.com>
2026-09-04 21:06:00 -0500

    Fix: modelo cliente estabilizado con ID aleatorio y sin conflictos de timestamps

--- auto_gestion-backend/src/models/OrdenServicio.js
+++ auto_gestion-backend/src/models/OrdenServicio.js
@@ -3,10 +3,8 @@ const sequelize = require('../config/database');
 
 const OrdenServicio = sequelize.define('OrdenServicio', {
   id: {
-    // 1. Ampliamos a BIGINT para soportar los 10 dígitos masivos
     type: DataTypes.BIGINT,
     primaryKey: true,
-    // 2. Quitamos la secuencia automática
     autoIncrement: false,
     field: 'orden_servicio_id'
   },
@@ -14,44 +12,39 @@ const OrdenServicio = sequelize.define('OrdenServicio', {
     type: DataTypes.DATEONLY,
     allowNull: false
   },
- tipo_orden: {
+  tipo_orden: {
     type: DataTypes.STRING,
-    field: 'tipo_orden' // Esto es lo único que importa para MySQL
-},
-vehiculo_id: {
+    field: 'tipo_orden'
+  },
+  vehiculo_id: {
     type: DataTypes.BIGINT,
     field: 'vehiculo_id'
-}
+  }
 }, {
   tableName: 'ordenes_servicio',
   timestamps: false,
-  
-  // 3. Añadimos el hook de generación aleatoria dispersa
   hooks: {
     beforeCreate: async (orden, options) => {
       let idExiste = true;
       let nuevoId;
 
       while (idExiste) {
-        // Genera un número aleatorio entero de 10 dígitos (entre 1000000000 y 9999999999)
         nuevoId = Math.floor(1000000000 + Math.random() * 9000000000);
-
-        // Validamos que no se repita en la tabla ordenes_servicio
         const duplicado = await OrdenServicio.findByPk(nuevoId);
         if (!duplicado) {
-          idExiste = false; // ID libre, rompemos bucle
+          idExiste = false;
         }
       }
 
-      // Asignamos el número generado al ID de la nueva orden
       orden.id = nuevoId;
     }
   }
 });
 
 OrdenServicio.associate = (models) => {
-  OrdenServicio.belongsTo(models.Vehiculo, { as: 'vehiculo', foreignKey: 'vehiculo_id' });
-  OrdenServicio.hasMany(models.ItemOrden, { as: 'items', foreignKey: 'orden_servicio_id' });
+  if (models && models.ItemOrden) {
+    OrdenServicio.hasMany(models.ItemOrden, { as: 'items', foreignKey: 'orden_servicio_id' });
+  }
 };
 
 module.exports = OrdenServicio;
